0ffe9d98cebd3cb0a557f1e514c836fa1173f1e0
EfyWidya <efyw36@gmail.com>
2015-06-14 16:17:47 +0700

    sudah

--- informasi.php
+++ informasi.php
@@ -6,37 +6,27 @@
 <!DOCTYPE html>
 
 <html lang="en">
+<html>
+
 <!-- isi tampilan -->
 <head>
-        <title> Sistem Informasi Presensi dan Jurnal Kelas Online</title>
-        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" 
-        <link href="css/style.css" rel="stylesheet" type="text/css" />
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        
-    </head>
-    
-
-<head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Sistem Informasi Presensi dan Jurnal Kelas Online - Informasi</title>
+<title>Sistem Informasi Presensi dan Jurnal Kelas- Informasi</title>
 <meta name="keywords" content="" />
 <meta name="description" content="" />
 
 <link href="css/tooplate_style.css" rel="stylesheet" type="text/css" />
 
-</head>
-<body>
 <div id="tooplate_body_wrapper">
 <div id="tooplate_wrapper">
 
 <div class="judul"> 
 		<p><h1><b><center><marquee><font color=blue>SELAMAT DATANG !!</font><center></marquee></b></h1></p>
-		</div>
-		<div class="judul"> .
-		<p><h1><b><center><font color=red>Sistem Informasi Presensi dan Jurnal Kelas Online</font><center></b></h1></p>
+		<p><h1><b><center><font color=red>Sistem Informasi Presensi dan Jurnal Kelas</font><center></b></h1></p>
 		</div>		
     
     <div id="tooplate_header">
+
         <div id="tooplate_menu">
             <ul>
                 <li><a href="index.php">Home</a></li>
@@ -44,19 +34,17 @@
             </ul>    	
         </div> <!-- end of tooplate_menu -->
     </div> <!-- end of forever header -->
-    
 
-    <body background = "image/hitam.png" >
 
     <div id="tooplate_middle_subpage">
-    	<h2>PERATURAN dan INFORMASI !!</h2>
+    	<p><h2>PERATURAN dan INFORMASI !!</h2></p>
+        <p>------------------------------------------------------------</p>
+        <p></p>
         <p>1. Silahkan melakukan absensi dan mengisi jurnal kelas pada setiap mata kuliah. Jika ingin melakukan absensi silahkan Login terlebih dahulu. Kemudian isi kehadiran anda. Jika ingin mengisi jurnal silahkan memilih login terlebih dahulu. Kemudian silahkan mengisi jurnal.</p>
 
 	</div> <!-- end of middle -->
     
     <div id="tooplate_main">
-    
-    	<div class="col_w450 float_l">
                     
         
 </div>
